418825563579b96361776f255c2fd0adfa9ae293
haducacarlo1220 <ralphhaduca@gmail.com>
2024-11-04 18:33:17 +0800

    initial commit

---
+++ userprofilr.js
@@ -0,0 +1,11 @@
+let firstName = "Ralph Carlo";
+let lastName = "Pano";
+let age = 18;
+let favoriteNumber = 20;
+let favoriteColor = "Black"
+
+console.log(firstName)
+console.log(lastName)
+console.log(age)
+console.log(favoriteNumber)
+console.log(favoriteColor)
